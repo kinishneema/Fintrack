ae44012c772c815d823dcfe000658418e97eb6c0
kinish123 <kinishneema7723@gmail.com>
2025-03-02 17:06:47 +0530

    Set up Express server with routing and static files

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const Router = require("./router/router");
 
 const app = express();
 
+// Middleware for parsing URL-encoded and JSON data
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json());
 
+// Set up routing
 app.use("/", Router);
 
+// Serve static files from the "public" directory
 app.use(express.static(`${__dirname}/public`))
 
 module.exports = app;
